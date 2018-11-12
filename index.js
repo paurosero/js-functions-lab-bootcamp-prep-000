9e27c759b08093796a321d11675ea4fb971651bf
Paul Rosero <paul_rosero@yahoo.com>
2018-11-12 23:56:30 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -6,3 +6,7 @@ function happyHolidaysTo(name){
   return "Happy holidays, you!"
   console.log('Happy holidays, ${you}')
 }
+function happyHolidayTo(holiday, name){
+  return "Happy holiday, you!"
+  console.log('Happy ${holiday}, ${name}!')
+}
