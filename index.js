82efda20764138a4d37485c3528eda3e4bf46997
Paul Rosero <paul_rosero@yahoo.com>
2018-11-13 00:03:20 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -11,6 +11,6 @@ function happyHolidayTo(holiday, name){
   console.log('Happy ${holiday}, ${name}!')
 }
 function holidayCountdown(holiday, days){
-  return "Its's 20 days until Mother's Day!"
-  console.log('its\'s ${days} days until ${holiday}!')
+  return "It's 20 days until Mother's Day!"
+  console.log('It\'s ${days} days until ${holiday}!')
 }
